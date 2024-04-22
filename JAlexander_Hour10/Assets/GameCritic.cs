d8f0337359abcd6a064ae0dbcb9f421d51eb8911
Joshalex1 <161536154+Joshalex1@users.noreply.github.com>
2024-04-21 22:29:20 -0700

    chaotic chaos

---
+++ JAlexander_Hour10/Assets/GameCritic.cs
@@ -0,0 +1,89 @@
+/*
+                                                             
+  ,ad8888ba,  88                                            
+ d8"'    `"8b 88                                            
+d8'           88                                            
+88            88,dPPYba,  ,adPPYYba,  ,adPPYba,  ,adPPYba,  
+88            88P'    "8a ""     `Y8 a8"     "8a I8[    ""  
+Y8,           88       88 ,adPPPPP88 8b       d8  `"Y8ba,   
+ Y8a.    .a8P 88       88 88,    ,88 "8a,   ,a8" aa    ]8I  
+  `"Y8888Y"'  88       88 `"8bbdP"Y8  `"YbbdP"'  `"YbbdP"'  
+                                                                                              
+ 
+                               
+88888888ba             88 88  
+88      "8b            88 88  
+88      ,8P            88 88  
+88aaaaaa8P' ,adPPYYba, 88 88  
+88""""""8b, ""     `Y8 88 88  
+88      `8b ,adPPPPP88 88 88  
+88      a8P 88,    ,88 88 88  
+88888888P"  `"8bbdP"Y8 88 88  
+                              
+ 
+ Is the game too easy or hard?
+It's both easy and hard because the balls are moving so fast that it's hard to
+hit them and the Balls hit every goal eventually when you do nothing.
+
+What would make it easier or harder?
+Once you hit a ball with your bumper, it slows down for a few seconds, which
+could make it easier to hit them and maybe adding obstacles near the goals could
+prevent balls from scoring on their own.
+
+What would give it a "wow" factor?
+Once the first ball scores, the goals start to alternate positions every few
+seconds.
+
+What parts of the game are fun? What parts of the game are tedious?
+Hiting the balls and watching them fly is still amusing and I'd say the chaos
+balls themselves could pose more of a challenge.
+
+
+Improvements:
+
+I added a terrain so the player sees more than empty space and I made bumpers
+for the goals so the balls don't just score by themselves.
+
+I changed the players bumper a little bit.
+
+I added some new Chaos balls.
+
+I Changed the players footsteps sounds with a water sound effect.
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+*/
---
+++ JAlexander_Hour10/Assets/Scripts/GoalScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalScript : MonoBehaviour
+{
+    public bool isSolved = false;
+    private void OnTriggerEnter(Collider collider)
+    {
+        GameObject collidedWith = collider.gameObject;
+        if (collidedWith.tag == gameObject.tag)
+        {
+            isSolved = true;
+            GetComponent<Light>().enabled = false;
+            Destroy(collidedWith);
+        }
+
+        if (collidedWith.CompareTag(gameObject.tag))
+        {
+            isSolved = true;
+            GetComponent<Light>().enabled = false;
+            Destroy(collidedWith);
+
+
+        }
+
+       
+
+  
+    }
+}
+
