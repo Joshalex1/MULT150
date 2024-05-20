ac5a590acb56963127a3efd7fead8310bed3bf06
Joshalex1 <161536154+Joshalex1@users.noreply.github.com>
2024-05-19 23:40:34 -0700

    gauntlet runner

---
+++ JAlexander_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,71 @@
+/*
+ 
+88888888ba  88888888ba  88        88 88        88  
+88      "8b 88      "8b 88        88 88        88  
+88      ,8P 88      ,8P 88        88 88        88  
+88aaaaaa8P' 88aaaaaa8P' 88        88 88aaaaaaaa88  
+88""""""8b, 88""""88'   88        88 88""""""""88  
+88      `8b 88    `8b   88        88 88        88  
+88      a8P 88     `8b  Y8a.    .a8P 88        88  
+88888888P"  88      `8b  `"Y8888Y"'  88        88  
+
+
+
+
+Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+The obstacles are pretty easy to avoid because the phase ability has no cool down,
+however, the obstacles do spawn right in front of the player so it can be challeging.
+
+
+
+What 2 changes would you do to make it easier or harder?
+I would have the obstacles spawn more clones across the map so it would be harder
+to grab powerups. I would also add a cooldown to the phase ability or make the
+powerups prevent you from phasing until the game time speeds up again.
+
+
+List 2 things that would give it a "wow" factor.
+Making the powerups move side to side could be interesting.
+Mabye making the entire gauntlet rotate in different directions and adding
+an evironment to show the player its rotating which could be distracting.
+
+
+What 2 parts of the game are fun?
+I enjoy the phase abitity mostly because of the material and moving the player
+and dodging is enjoyable.
+
+
+What 2 parts of the game are tedious?
+The obstacles spawning in a line is pretty repetitive.
+The powerups sometimes spawn right next to eachother which makes it too easy to
+pick them up.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
